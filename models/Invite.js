6722bd1014b2c210921e3918e4a8e2721f89c064
Christoffer Assgård <a.christoffer@outlook.com>
2016-12-27 10:21:24 +0100

    Changed some default text and added token generation

--- models/Invite.js
+++ models/Invite.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 
 const inviteSchema = new mongoose.Schema({
@@ -7,6 +8,19 @@ const inviteSchema = new mongoose.Schema({
 
 }, { timestamps: true });
 
+
+/**
+ * Generate token middleware
+ */
+inviteSchema.pre('save', function save(next) {
+    const invite = this;
+    crypto.randomBytes(16, (err, buf) => {
+        if (err) { return next(err); }
+        const token = buf.toString('hex');
+        invite.token = token;
+        next();
+    });
+});
 
 const Invite = mongoose.model('Invite', inviteSchema);
 
